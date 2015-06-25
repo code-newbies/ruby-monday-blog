51c3b2d9fec50b6b6bcc49146d83403e55c2141f
Calvyn82 <claytonflesher@gmail.com>
2015-06-24 20:01:27 -0500

    created user authentication

--- ruby-monday-blog/app/controllers/application_controller.rb
+++ ruby-monday-blog/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
 end
--- ruby-monday-blog/db/schema.rb
+++ ruby-monday-blog/db/schema.rb
@@ -11,16 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150620040603) do
-=======
-ActiveRecord::Schema.define(version: 0) do
->>>>>>> origin/master
+ActiveRecord::Schema.define(version: 20150625005137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
@@ -28,6 +23,22 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "updated_at", null: false
   end
 
-=======
->>>>>>> origin/master
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
