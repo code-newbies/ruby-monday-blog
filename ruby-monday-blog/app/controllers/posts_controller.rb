83b8ba645f129a19a7421f323f13add415e61619
farishkash <farishkash@hotmail.com>
2015-07-13 14:59:44 -0700

    Index Page for all posts done

--- ruby-monday-blog/app/controllers/posts_controller.rb
+++ ruby-monday-blog/app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+
+  def index
+  @posts = Post.all
+  end
+
   def new
     @post = Post.new
     @tag = @post.tags.build
--- ruby-monday-blog/app/controllers/tags_controller.rb
+++ ruby-monday-blog/app/controllers/tags_controller.rb
@@ -3,23 +3,4 @@ class TagsController < ApplicationController
   def new
   @tag = Tag.new
   end
-
-  def create
-    @tag = Tag.new(tag_params)
-    if @tag.save
-      flash[:notice] = "#{@tag.content} has been added."
-    redirect_to @tag
-    else
-      render 'new'
-    end
-  end
-
-  def show
-    @tag = Tag.find(params[:id])
-  end
-
-  private
-  def tag_params
-    params.require(:tag).permit(:content)
-  end
 end
