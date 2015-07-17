a16e667a0a1a6e665cb073148c64d3d6a4664609
Ross Waguespack <rosswaguespack@gmail.com>
2015-07-16 21:50:42 -0500

    Changed Seed data to seeds.rb

--- ruby-monday-blog/db/seeds.rb
+++ ruby-monday-blog/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+namespace :db do
+  desc "Add posts data to views"
+  task :seed => :environment do
+    require 'populator'
+    require 'faker'
+
+    tags_list = ["Ruby on Rails", "Ruby", "JavaScript", "Angular"]
+    
+    User.populate 20 do |user|
+      user.email    = Faker::Internet.email
+      user.encrypted_password = Faker::Internet.password(8)
+      user.sign_in_count = 10..100
+      Post.populate 10 do |post|
+        post.title = Populator.words(1..3).titleize
+        post.body  = Populator.sentences(10..20)
+      end
+    end
+
+    Tag.populate 20 do |tag|
+      tag.content = ["Ruby on Rails", "Ruby", "JavaScript", "Ruby Gems", "Angular", "Meteor"]
+    end
+  end
+end
+
