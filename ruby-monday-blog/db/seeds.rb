17b043dc9264f32c226ff056e3e6f7c9b95a7b8b
Calvyn82 <claytonflesher@gmail.com>
2015-07-29 16:32:09 -0500

    Seeded database with PostTag objects

--- ruby-monday-blog/db/seeds.rb
+++ ruby-monday-blog/db/seeds.rb
@@ -3,10 +3,6 @@ namespace :db do
   task :seed => :environment do
     require 'populator'
     require 'faker'
-
-    Tag.populate 20 do |tag|
-      tag.content = ["Ruby on Rails", "Ruby", "JavaScript", "Ruby Gems", "Angular", "Meteor"]
-    end
     
     User.populate 20 do |user|
       user.email    = Faker::Internet.email
@@ -16,6 +12,14 @@ namespace :db do
         post.title = Populator.words(1..3).titleize
         post.body  = Populator.sentences(10..20)
       end
+    end
+
+    Tag.populate 20 do |tag|
+      tag.content = ["Ruby on Rails", "Ruby", "JavaScript", "Ruby Gems", "Angular", "Meteor"]
+    end
+
+    Tag.all.zip(Post.all).each do |pair|
+      PostTag.create(tag: pair.first, post: pair.last)
     end
   end
 end
