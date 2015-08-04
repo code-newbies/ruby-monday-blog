48bbd4d1b0f46ec898d13e266678d6ec62843be4
matugm <matugm@gmail.com>
2015-08-04 21:34:23 +0200

    Refactor test

--- ruby-monday-blog/spec/controllers/posts_controller_spec.rb
+++ ruby-monday-blog/spec/controllers/posts_controller_spec.rb
@@ -1,17 +1,24 @@
 require "rails_helper"
 
 describe PostsController do
-  it "doesn't create a new tag if it already exists" do
-    params = {
-      post: {
-        body:  'lorem',
-        title: 'ipsum',
-        tags_attributes: [{ content: 'Ruby' }]
-      }
+  let(:params) do
+  {
+    post: {
+      body:  'lorem',
+      title: 'ipsum',
+      tags_attributes: [{ content: 'Ruby' }]
     }
+  }
+  end
 
+  it "creates a new tag if it doesn't exist" do
     expect { post :create, params }
       .to change{ Tag.count }.by (1)
+  end
+
+  it "can reuse a tag that already exists" do
+    # Create the first tag
+    post :create, params
 
     expect { post :create, params }
       .to_not change{ Tag.count }
