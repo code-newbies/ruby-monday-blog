49f8c6cd64ea9bbc2a85620b8a7ff8b9c102bc03
Allan Reyes <allanbreyes@gmail.com>
2015-08-11 13:56:51 -0500

    add have_link matcher

--- ruby-monday-blog/spec/features/member_can_navigate_to_post_from_index_spec.rb
+++ ruby-monday-blog/spec/features/member_can_navigate_to_post_from_index_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "member can navigate to post from post index", type: :feature do
   scenario "user visits post index and clicks on a post headline" do
     # log_in_as user
     visit posts_path
-    expect(page).to have_css "a", text: 'My Post'
+    expect(page).to have_link 'My Post'
     click_on 'My Post'
     expect(current_path).to match post_path(post)
     expect(page).to have_content 'My Content'
