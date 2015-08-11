14123815814dd25749d8b2981d23fdc0ff775998
Allan Reyes <allanbreyes@gmail.com>
2015-08-11 11:51:08 -0500

    clean up feature spec for post navigation

--- ruby-monday-blog/spec/features/member_can_navigate_to_post_from_index_spec.rb
+++ ruby-monday-blog/spec/features/member_can_navigate_to_post_from_index_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "member can navigate to post from post index", type: :feature do
-  before :each do
-    # replace with fixtures
-    @post = Post.create(title: 'My Post', body: 'My Content')
-  end
+  let!(:post) { Post.create(title: 'My Post', body: 'My Content') }
 
   scenario "user visits post index and clicks on a post headline" do
     # log_in_as user
     visit posts_path
     expect(page).to have_css "a", text: 'My Post'
     click_on 'My Post'
-    expect(current_url).to match post_url(@post)
+    expect(current_path).to match post_path(post)
     expect(page).to have_content 'My Content'
   end
 end
