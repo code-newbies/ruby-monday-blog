d91d3a4ef39d2c5ef5054d97048b9ac74cf95bb8
Creativisionist <creativisionist@gmail.com>
2015-08-17 23:19:30 -0500

    add first name and last name fields to devise

--- ruby-monday-blog/db/schema.rb
+++ ruby-monday-blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150811195313) do
+ActiveRecord::Schema.define(version: 20150818035637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20150811195313) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
