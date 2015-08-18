6b09cc22984f024a1119efcea022c1a697fc1d06
Abinoam Praxedes Marques Jr <abinoam@gmail.com>
2015-08-18 12:41:04 -0300

    Add spec - To have post's author set to the current user
    It's necessary to save the current user into the post's author
    attribute at creation time

--- ruby-monday-blog/spec/controllers/posts_controller_spec.rb
+++ ruby-monday-blog/spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 describe PostsController do
+  let!(:user) { create(:user) }
+
   let(:params) do
   {
     post: {
@@ -12,13 +14,14 @@ describe PostsController do
   end
 
   it "creates a new tag if it doesn't exist" do
-    sign_in
+    sign_in user
+
     expect { post :create, params }
       .to change{ Tag.count }.by (1)
   end
 
   it "can reuse a tag that already exists" do
-    sign_in
+    sign_in user
 
     # Create a new tag
     post :create, params
@@ -27,13 +30,21 @@ describe PostsController do
       .to_not change{ Tag.count }
   end
 
+  it "assigns current user as post's author" do
+    sign_in user
+
+    post :create, params
+
+    expect(Post.last.author).to eq user
+  end
+
   it 'redirects the user from posts#new to posts#index if not logged in' do
     get :new
     expect(response).to redirect_to posts_path
   end
 
   it 'renders posts#new for a logged in user' do
-    sign_in
+    sign_in user
     get :new
     expect(request.path).to match new_post_path
   end
