c175801898f651ac5859913df8a48f333e5bd92f
Abinoam Praxedes Marques Jr <abinoam@gmail.com>
2015-08-18 13:03:49 -0300

    Save post's author at PostsController#create

--- ruby-monday-blog/app/controllers/posts_controller.rb
+++ ruby-monday-blog/app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.author = current_user
 
     if @post.save
       flash[:notice] = "Post has been created."
