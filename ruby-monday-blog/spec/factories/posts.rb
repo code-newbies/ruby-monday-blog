31afff81f4aa2a53b53ab1b0030d65f362c5e439
Allan Reyes <allanbreyes@gmail.com>
2015-08-25 02:13:41 -0500

    move faker to rails helper + get member_can_navigate_to_post_from_index feature spec to pass

--- ruby-monday-blog/spec/factories/posts.rb
+++ ruby-monday-blog/spec/factories/posts.rb
@@ -16,8 +16,6 @@
 
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-require 'faker'
-
 FactoryGirl.define do
   factory :post do
     title { Faker::Lorem.sentence }
--- ruby-monday-blog/spec/rails_helper.rb
+++ ruby-monday-blog/spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'faker'
 require 'pundit/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
