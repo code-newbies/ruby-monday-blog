97fce313da43c4b0321fb4dfe82826af5957242b
Creativisionist <creativisionist@gmail.com>
2015-08-28 14:49:18 -0500

    add first_name last_name validation if author; add tests for author and reader

--- ruby-monday-blog/spec/models/user_spec.rb
+++ ruby-monday-blog/spec/models/user_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of :first_name }
+  let(:author) { User.new(first_name: 'First', last_name: 'Last', email: 'test@example.com', password: 'password', role: :author) }
+  let(:reader) { User.new(first_name: nil, last_name: nil, email: 'test@example.com', password: 'password', role: :reader) }
 
-  it { should validate_presence_of :last_name }
+  describe 'validations' do
+    context 'when user is an author' do
+      it 'validates presence of first name and last name' do
+        expect(author).to be_valid
+      end
+    end
+
+    context 'when user is a reader' do
+      it 'should not validate presence of names' do
+        expect(reader).to be_valid
+      end
+    end
+  end
 end
