47ec5f4df16d437b2b4ac3b611f55cb1b358b58b
Jason Data <jason.data@roadbytes.me>
2016-01-22 10:19:38 +0900

    my local setup changes

--- ruby-monday-blog/config/initializers/devise.rb
+++ ruby-monday-blog/config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = '7ef4ef91854c7289aa02f2ee891e2cd87c2149dad5fe03d668f12f7f72ae9683d2f188030087f8268e4edee34e68e80ef65adbe58d7a03327b0b2be3799f8a0a'
+  config.secret_key = '7ef4ef91854c7289aa02f2ee891e2cd87c2149dad5fe03d668f12f7f72ae9683d2f188030087f8268e4edee34e68e80ef65adbe58d7a03327b0b2be3799f8a0a'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
